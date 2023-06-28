020fb9238ad8a1a8e3377691b71bd01827514d4c
cooljosh96 <uzojosh321@yahoo.com>
2023-06-28 08:25:21 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,25 +5,17 @@
  */
 int main(void)
 {
-
 int n, m;
-
-
 for (n = 48; n <= 56; n++)
-
 {
-
 for (m = 49; m <= 57; m++)
 {
 if (m > n)
 {
 putchar(n);
 putchar(m);
-
 if (n != 56 || m != 57)
-
 {
-
 putchar(',');
 
 putchar(' ');
@@ -34,4 +26,3 @@ putchar(' ');
 putchar('\n');
 return (0);
 }
-
